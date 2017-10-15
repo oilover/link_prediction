952f4694d86dbb3cf684baeab630315f426a9fe5
Liu Huang <oilover@qq.com>
2017-10-16 00:59:51 +0800

    Small work

--- Ext_Ja.py
+++ Ext_Ja.py
@@ -5,9 +5,10 @@
 import time,random
 import sys, pickle
 import networkx as nx
-name = 'ca-cit-HepPh'
+name = 'digg-friends' #'ca-cit-HepPh'
 #'internet-growth.txt' #'flickr-growth-sorted.txt''ca-cit-HepPh' 
 file_name = name+'/'+name+'.txt' 
+Ja_file = file_name[:-4] + '-Ja.txt'
 CN_file = file_name[:-4] + '-common_neighbor.txt'
 distance_file = file_name[:-4] + '--distance.txt'
 AA_file = file_name[:-4] + '--AA.txt'
@@ -85,14 +86,21 @@ if __name__ == '__main__':
 			newG.add_edge(u,v)	
 			# if i==edgen: break		
 		i+=1
+	for u in G.nodes(): G.node[u]['community'] = 0
+	Ja = nx.jaccard_coefficient(G, sample_missing_edges(newG,G) )
+	pickle.dump(Ja, open(to_negative(Ja_file),'w'))
+
 	CN = nx.cn_soundarajan_hopcroft(G, newG.edges())  # long time
 	CN = list(CN)
 	pickle.dump(CN, open(CN_file,'w'))
-	CN = nx.cn_soundarajan_hopcroft(G, sample_missing_edges(newG,G))
+
+	CN = list(nx.cn_soundarajan_hopcroft(G, sample_missing_edges(newG,G)))
 	pickle.dump(CN, open(to_negative(CN_file),'w'))
-	AA = nx.adamic_adar_index(G, newG.edges())
+
+	AA = list(nx.adamic_adar_index(G, newG.edges()) )
 	pickle.dump(AA, open(AA_file,'w'))
-	AA = nx.adamic_adar_index(G, sample_missing_edges(newG,G))
+
+	AA = list(nx.adamic_adar_index(G, sample_missing_edges(newG,G)) )
 	pickle.dump(AA, open(to_negative(AA_file),'w'))
 
 	prediction = sorted(predict, key=lambda x:x[-1], reverse=True)  #key=lambda x:x[-1]
