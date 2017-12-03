df315650b2ff7f9374df9fa228ce99500ff4ce8b
黄柳 <1310421753@qq.com>
2017-12-03 13:56:33 +0800

    more experiments

--- STC.py
+++ STC.py
@@ -76,17 +76,18 @@ def updateOpens(opens, e):
 if __name__ == '__main__':
 
     #name = sys.argv[1]
-    name = 'BM_tags' #'lastFM_tags'                
+    name = 'FB_circles' #'youtube_10000' #'BM_tags' #'lastFM_tags'                
 
     G, init_sets = pickle.load(open(os.path.join('UnderlyingNetwork', name +'.pkl'),'rb'))
-    
+    print 'Data: ', name
     print 'number of nodes and edges in the graph:'
     print G.number_of_nodes(), G.number_of_edges()
     sets, Gteams = getDataGraph(init_sets, G)
     print 'number of sets:', len(sets)
     
     opens, index = getAllOpenTriangles(G, sets)
-    print 'initial number of open (violated) triangles:', sum([len(i) for i in opens.values()])/2
+    all_open = sum([len(i) for i in opens.values()])/2
+    print 'initial number of open (violated) triangles:', all_open
         
     Strong = set([(i,j) if i < j else (j,i) for (i,j) in G.edges()])
     Weak = set()
@@ -117,7 +118,10 @@ if __name__ == '__main__':
             unremovable.add(to_remove)
 
     print 'running time:', time.time() - tic
-    print 'resulting number of violations: ', (sum([len(i) for i in opens.values()]))
+    res = sum([len(i) for i in opens.values()])/2
+    print 'resulting number of violations: ', res
+    print 'violation rate: (b in paper)', 1.0*res / all_open
     print 'number of strong and weak edges:'
     print len(Strong), len(Weak)
-    pickle.dump((Gteams, sets, Strong, Weak), open('out/' + name + '.p2', "wb"))
+    print 'Strong edge ratio (s in paper): ', 1.0*len(Strong) / G.number_of_edges()
+    pickle.dump((Gteams, sets, Strong, Weak), open('out/' + name + time.ctime(time.time()) + '.p2', "wb"))
